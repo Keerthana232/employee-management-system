1de5918dbc35384cf6a2c29942b3793ac27e1ebb
Keerthana232 <keerthanasundaram23@Gmail.com>
2026-07-30 08:58:43 +0530

    Fix CORS to allow Vercel frontend

--- backend/EmployeeManagementSystem/src/main/java/com/employee/EmployeManagemnetSystem/config/CorsConfig.java
+++ backend/EmployeeManagementSystem/src/main/java/com/employee/EmployeManagemnetSystem/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:5500", "https://employee-management-system-zeta-gray.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
